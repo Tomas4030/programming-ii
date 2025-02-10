9dd1f3cf230a5442570bff549b542fbe8a250caf
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2025-02-10 21:46:04 +0000

    Add files via upload

--- script/sessao4/teste.js
+++ script/sessao4/teste.js
@@ -4,7 +4,7 @@ try {
 
 
     const rows = csvData.split('\n');
-    const headers = rows[0].split(','); // ["name", "email", "age"]
+    const headers = rows[0].split(','); 
     const data = rows.slice(1).map(row=> {
         const values = row.split(',');
         return {
